433e82f413caff264ec885c00b24b46d6544f403
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2023-03-26 15:14:31 +0200

    :arrow_up: add tailwindcss to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main>
-            <h1>Random plouf</h1>
+            <h1 className="text-center text-3xl font-bold">Random plouf</h1>
         </main>
     );
 }

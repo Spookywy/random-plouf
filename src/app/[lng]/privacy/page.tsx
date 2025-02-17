7d5eb6faed289bff6af4727f9b20ad21b8336f4a
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2025-02-17 23:57:30 +0100

    📝 add legal page for the mobile APP

---
+++ src/app/[lng]/privacy/page.tsx
@@ -0,0 +1,60 @@
+export default function Privacy() {
+  return (
+    <main className="flex flex-col gap-4 p-5">
+      <h1 className="text-xl font-semibold">Privacy Policy</h1>
+      <p>
+        At RanDOM Plouf, we respect your privacy and are committed to protecting
+        the personal information you may provide through our app. This privacy
+        policy explains how we handle your data.
+      </p>
+      <p className="font-semibold">1. Data Collection</p>
+      <p>
+        We do not collect any personal data from users of the RanDOM Plouf app.
+        You can use the app without providing any personal information.
+      </p>
+      <p className="font-semibold">2. Third-Party Services</p>
+      <p>
+        Our app does not use any third-party services that collect data. You can
+        be assured that no external parties are accessing or tracking your
+        information.
+      </p>
+      <p className="font-semibold">3. Data Storage</p>
+      <p>
+        We do not store any data. All interactions within the app are processed
+        in real-time without retaining any personal information.
+      </p>
+      <p className="font-semibold">4. Data Sharing</p>
+      <p>
+        We do not share any data with third parties. Your privacy is our
+        priority, and no data is shared, sold, or otherwise disclosed.
+      </p>
+      <p className="font-semibold">5. User Rights</p>
+      <p>
+        Since we do not collect or store any personal data, there is no personal
+        information to manage or delete. You are free to use the app without
+        concern about your data being retained.
+      </p>
+      <p className="font-semibold">6. Security</p>
+      <p>
+        We take security seriously. Since no data is collected, there are no
+        security risks related to your personal information in our app.
+      </p>
+      <p className="font-semibold">7. Children’s Privacy</p>
+      <p>
+        RanDOM Plouf is not intended for children under the age of 13, and we do
+        not knowingly collect any personal data from children.
+      </p>
+      <p className="font-semibold">8. Changes to this Policy</p>
+      <p>
+        We reserve the right to update or change this privacy policy. Any
+        changes will be reflected here, and we encourage you to check this page
+        regularly.
+      </p>
+      <p className="font-semibold">Contact Us</p>
+      <p>
+        If you have any questions about this privacy policy or your use of the
+        RanDOM Plouf app, you can contact us at: valandxp@gmail.com
+      </p>
+    </main>
+  );
+}
